c3b43b4ae60df37d34465cc896aa73ea57310ded
tervingo <keltivas@gmail.com>
2025-02-07 21:30:29 +0100

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/florplay',
+  integrations: [react()],
+  outDir: './dist', // Esto es redundante porque ya es el valor por defecto
+  vite: {
+    server: {
+      fs: {
+        allow: [
+          'C:/Users/j4alo/Dropbox/Eltomalturta/florplay'
+        ]
+      }
+    }
+  }
+});
